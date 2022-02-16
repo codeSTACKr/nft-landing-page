8cd3c4a393e379362cde6b1633c5612cd6ae2e5e
RiveraAlan <alan.rivera5@upr.edu>
2022-02-16 00:32:16 -0400

    Finalized character creator

--- js/nft-creator.js
+++ js/nft-creator.js
@@ -1,6 +1,39 @@
-function changeTrait(id) {
-    var selected = document.getElementById(`select_${id}`).value;
-    var trait = document.getElementById(id);
-console.log(selected + id);
-    trait.src = `../images/pipi-kun/layers/${id} ${selected}.png`;
+const face = 5;
+const head = 6;
+const torso = 2;
+const shoes = 2;
+function changeTrait(operator) {
+    var selected = document.getElementById(`trait`).value;
+    var img = document.getElementById(selected);
+    var value = document.getElementById(selected).alt;
+    var trait = parseInt(value);
+    switch (selected) {
+        case "face":
+            var limit = face;
+            break;
+        case "head":
+            var limit = head;
+            break;
+        case "torso":
+            var limit = torso;
+            break;
+        case "shoes":
+            var limit = shoes;
+            break;
+        default:
+        break;
+    }
+
+    if (operator == "plus" && trait != limit){
+        var id = trait + 1;
+    } else if (operator == "minus" && trait != 1) {
+        var id = trait - 1;
+    } else if (operator == "plus" && trait == limit) {
+        var id = 1;
+    } else if (operator == "minus" && trait == 1) {
+        var id = limit;
+    }
+
+        img.alt = id;
+        img.src = `../images/pipi-kun/layers/${selected} ${id}.png`;
 }
