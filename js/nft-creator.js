0462647dc7adcf2b4ea47631d030e10b9ea1b5c3
RiveraAlan <alan.rivera5@upr.edu>
2022-02-20 14:40:42 -0400

    Updated Accessories
    Note : some accessories don't match sizing

--- js/nft-creator.js
+++ js/nft-creator.js
@@ -1,7 +1,8 @@
-const face = 5;
-const head = 6;
-const torso = 2;
-const shoes = 2;
+const face = 8;
+const head = 8;
+const torso = 8;
+const shoes = 6;
+const outfits = 10;
 function changeTrait(operator) {
     var selected = document.getElementById(`trait`).value;
     var img = document.getElementById(selected);
@@ -13,12 +14,39 @@ function changeTrait(operator) {
             break;
         case "head":
             var limit = head;
+            if (document.getElementById("head").classList.contains("hide-layer")) {
+                document.getElementById("head").classList.toggle("hide-layer");
+                document.getElementById("torso").classList.toggle("hide-layer");
+                document.getElementById("shoes").classList.toggle("hide-layer");
+                document.getElementById("outfit").classList.toggle("hide-layer");
+            }
             break;
         case "torso":
             var limit = torso;
+            if (document.getElementById("torso").classList.contains("hide-layer")) {
+                document.getElementById("head").classList.toggle("hide-layer");
+                document.getElementById("torso").classList.toggle("hide-layer");
+                document.getElementById("shoes").classList.toggle("hide-layer");
+                document.getElementById("outfit").classList.toggle("hide-layer");
+            }
             break;
         case "shoes":
             var limit = shoes;
+            if (document.getElementById("shoes").classList.contains("hide-layer")) {
+                document.getElementById("head").classList.toggle("hide-layer");
+                document.getElementById("torso").classList.toggle("hide-layer");
+                document.getElementById("shoes").classList.toggle("hide-layer");
+                document.getElementById("outfit").classList.toggle("hide-layer");
+            }
+            break;
+        case "outfit":
+            var limit = outfits;
+            if (document.getElementById("outfit").classList.contains("hide-layer")){
+                document.getElementById("head").classList.toggle("hide-layer");
+                document.getElementById("torso").classList.toggle("hide-layer");
+                document.getElementById("shoes").classList.toggle("hide-layer");
+                document.getElementById("outfit").classList.toggle("hide-layer");
+            }
             break;
         default:
         break;
