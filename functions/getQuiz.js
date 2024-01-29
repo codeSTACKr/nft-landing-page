72436e073e55bf3c9e3ba26f81946a3d24f4c371
-_- <70543052+ekhriz@users.noreply.github.com>
2024-01-29 15:16:15 -0500

    d14

--- functions/getQuiz.js
+++ functions/getQuiz.js
@@ -5,22 +5,23 @@ exports.handler = async (event, context) => {
   const { quizId } = event.queryStringParameters;
 
   try {
-    const response = await fetch(`${process.env.PLANET_SCALE_URL}/${quizId}`);
-    
-    if (!response.ok) {
-      throw new Error(`Failed to fetch quiz data. Status: ${response.status}`);
-    }
+    console.log('Fetching quiz data for quizId:', quizId);
 
+    const response = await fetch(`https://alienznbotz.xyz/.netlify/functions/getQuiz/${quizId}`);
     const quizData = await response.json();
+
+    console.log('Quiz data fetched successfully:', quizData);
 
     return {
       statusCode: 200,
       body: JSON.stringify(quizData),
     };
   } catch (error) {
+    console.error('Error fetching quiz data:', error);
+
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: `Internal Server Error: ${error.message}` }),
+      body: JSON.stringify({ error: 'Internal Server Error' }),
     };
   }
 };
