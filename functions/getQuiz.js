697cac075639d69eb6de8034a3463ba24abc216b
-_- <70543052+ekhriz@users.noreply.github.com>
2024-01-29 15:21:19 -0500

    d15

--- functions/getQuiz.js
+++ functions/getQuiz.js
@@ -1,13 +1,21 @@
 // functions/getQuiz.js
+
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   const { quizId } = event.queryStringParameters;
 
   try {
-    console.log('Fetching quiz data for quizId:', quizId);
-
     const response = await fetch(`https://alienznbotz.xyz/.netlify/functions/getQuiz/${quizId}`);
+    
+    if (!response.ok) {
+      console.error(`Failed to fetch quiz data. Status: ${response.status}`);
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: 'Failed to fetch quiz data' }),
+      };
+    }
+
     const quizData = await response.json();
 
     console.log('Quiz data fetched successfully:', quizData);
