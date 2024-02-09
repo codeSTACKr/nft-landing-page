f795f1c66761e4c7c55a09c3240b20ad60ccb80f
ekhriz <70543052+ekhriz@users.noreply.github.com>
2024-02-09 11:02:48 -0500

    new

--- compTIA/js/sec+.js
+++ compTIA/js/sec+.js
@@ -7,136 +7,136 @@ document.addEventListener('DOMContentLoaded', function () {
     // Trigger click event on the menu link to show the menu initially
     document.getElementById("menu").click();
 
-// Add JavaScript to toggle the active class on click
-document.getElementById("menu").addEventListener("click", function () {
-    document.getElementById("quiz-links").classList.toggle("active");
-});
+    // Add JavaScript to toggle the active class on click
+    document.getElementById("menu").addEventListener("click", function () {
+        document.getElementById("quiz-links").classList.toggle("active");
+    });
 
-// Add event listeners to quiz links
-document.getElementById('quiz1').addEventListener('click', () => {
-    loadQuiz(1);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    // Add event listeners to quiz links
+    document.getElementById('quiz1').addEventListener('click', () => {
+        loadQuiz(1);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz2').addEventListener('click', () => {
-    loadQuiz(2);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz2').addEventListener('click', () => {
+        loadQuiz(2);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz3').addEventListener('click', () => {
-    loadQuiz(3);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz3').addEventListener('click', () => {
+        loadQuiz(3);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz4').addEventListener('click', () => {
-    loadQuiz(4);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz4').addEventListener('click', () => {
+        loadQuiz(4);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz5').addEventListener('click', () => {
-    loadQuiz(5);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz5').addEventListener('click', () => {
+        loadQuiz(5);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz6').addEventListener('click', () => {
-    loadQuiz(6);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz6').addEventListener('click', () => {
+        loadQuiz(6);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz7').addEventListener('click', () => {
-    loadQuiz(7);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz7').addEventListener('click', () => {
+        loadQuiz(7);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz8').addEventListener('click', () => {
-    loadQuiz(8);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz8').addEventListener('click', () => {
+        loadQuiz(8);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz9').addEventListener('click', () => {
-    loadQuiz(9);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz9').addEventListener('click', () => {
+        loadQuiz(9);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz10').addEventListener('click', () => {
-    loadQuiz(10);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz10').addEventListener('click', () => {
+        loadQuiz(10);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz11').addEventListener('click', () => {
-    loadQuiz(11);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz11').addEventListener('click', () => {
+        loadQuiz(11);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz12').addEventListener('click', () => {
-    loadQuiz(12);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz12').addEventListener('click', () => {
+        loadQuiz(12);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-document.getElementById('quiz13').addEventListener('click', () => {
-    loadQuiz(13);
-    closeMenu();
-    toggleQuizOptionsAlignment(false);
-});
+    document.getElementById('quiz13').addEventListener('click', () => {
+        loadQuiz(13);
+        closeMenu();
+        toggleQuizOptionsAlignment(false);
+    });
 
-function closeMenu() {
-    // Close the menu by removing the active class
-    document.getElementById("quiz-links").classList.remove("active");
-}
+    function closeMenu() {
+        // Close the menu by removing the active class
+        document.getElementById("quiz-links").classList.remove("active");
+    }
 
-async function loadQuiz(quizId) {
-    try {
-        const response = await fetchQuestions(quizId);
-        if (response.ok) {
-            const questions = await response.json();
-            displayQuestions(questions);
-            toggleQuizOptionsAlignment(true); // Align quiz options to the left
+    async function loadQuiz(quizId) {
+        try {
+            const response = await fetchQuestions(quizId);
+            if (response.ok) {
+                const questions = await response.json();
+                displayQuestions(questions);
+                toggleQuizOptionsAlignment(true); // Align quiz options to the left
 
-            // Change the URL without triggering a full page reload
-            const quizName = `Quiz-${quizId}`;
-            const newUrl = window.location.href.split('#')[0] + '#' + quizName;
-            history.pushState(null, null, newUrl);
+                // Change the URL without triggering a full page reload
+                const quizName = `Quiz-${quizId}`;
+                const newUrl = window.location.href.split('#')[0] + '#' + quizName;
+                history.pushState(null, null, newUrl);
 
-        } else {
-            throw new Error(`Failed to fetch questions: ${response.statusText}`);
+            } else {
+                throw new Error(`Failed to fetch questions: ${response.statusText}`);
+            }
+        } catch (error) {
+            console.error('Error loading quiz:', error.message);
         }
-    } catch (error) {
-        console.error('Error loading quiz:', error.message);
     }
-}
 
-function toggleQuizOptionsAlignment(alignLeft) {
-    const quizOptions = document.getElementById('quiz-links');
-    if (alignLeft) {
-        quizOptions.classList.add('quiz-options');
-    } else {
-        quizOptions.classList.remove('quiz-options');
+    function toggleQuizOptionsAlignment(alignLeft) {
+        const quizOptions = document.getElementById('quiz-links');
+        if (alignLeft) {
+            quizOptions.classList.add('quiz-options');
+        } else {
+            quizOptions.classList.remove('quiz-options');
+        }
     }
-}
 
-async function fetchQuestions(quizId) {
-    const apiUrl = 'https://alienznbotz.xyz/.netlify/functions/getQuiz';
-    return await fetch(`${apiUrl}?quizId=${quizId}`);
-}
+    async function fetchQuestions(quizId) {
+        const apiUrl = 'https://alienznbotz.xyz/.netlify/functions/getQuiz';
+        return await fetch(`${apiUrl}?quizId=${quizId}`);
+    }
 
-function displayQuestions(questions) {
-    let quizHtml = ''; /* Remove the initial heading */
+    function displayQuestions(questions) {
+        let quizHtml = ''; /* Remove the initial heading */
 
-    questions.forEach((question, index) => {
-        const correctOptionText = getOptionText(question.correctOption, question);
-        quizHtml += `
+        questions.forEach((question, index) => {
+            const correctOptionText = getOptionText(question.correctOption, question);
+            quizHtml += `
             <div class="quiz-container">
                 <p>Q${index + 1}: ${question.questionText}</p>
                 <ul>
@@ -150,31 +150,32 @@ function displayQuestions(questions) {
                 <p class="explanation" id="explanation-${index}">Explanation: ${question.explanation}</p>
             </div>
         `;
-    });
+        });
 
-    quizContainer.innerHTML = quizHtml;
-}
-
-function showAnswer(index) {
-    const correctOptionElement = document.getElementById(`correct-option-${index}`);
-    const explanationElement = document.getElementById(`explanation-${index}`);
-
-    // Toggle the visibility of correct option and explanation
-    correctOptionElement.style.display = (correctOptionElement.style.display === 'none' || !correctOptionElement.style.display) ? 'block' : 'none';
-    explanationElement.style.display = (explanationElement.style.display === 'none' || !explanationElement.style.display) ? 'block' : 'none';
-}
-
-function getOptionText(option, question) {
-    switch (option) {
-        case 1:
-            return question.option1;
-        case 2:
-            return question.option2;
-        case 3:
-            return question.option3;
-        case 4:
-            return question.option4;
-        default:
-            return 'Unknown Option';
+        quizContainer.innerHTML = quizHtml;
     }
-}
+
+    function showAnswer(index) {
+        const correctOptionElement = document.getElementById(`correct-option-${index}`);
+        const explanationElement = document.getElementById(`explanation-${index}`);
+
+        // Toggle the visibility of correct option and explanation
+        correctOptionElement.style.display = (correctOptionElement.style.display === 'none' || !correctOptionElement.style.display) ? 'block' : 'none';
+        explanationElement.style.display = (explanationElement.style.display === 'none' || !explanationElement.style.display) ? 'block' : 'none';
+    }
+
+    function getOptionText(option, question) {
+        switch (option) {
+            case 1:
+                return question.option1;
+            case 2:
+                return question.option2;
+            case 3:
+                return question.option3;
+            case 4:
+                return question.option4;
+            default:
+                return 'Unknown Option';
+        }
+    }
+});
